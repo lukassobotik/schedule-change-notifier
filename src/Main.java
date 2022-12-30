8fa0f4bc45ff33e5e56e31ec0648a625949186d3
PuckyEU <osmset@gmail.com>
2022-12-30 17:55:06 +0100

    Removed Timer for the usage of CRON

--- src/Main.java
+++ src/Main.java
@@ -1,71 +1,107 @@
 import io.github.cdimascio.dotenv.Dotenv;
 import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.time.Month;
 import java.util.*;
 
 public class Main {
-    static ArrayList<String> HTMLTables = new ArrayList<>();
+    static List<String> HTMLTables = new ArrayList<>();
     static Set<String> recipients = new HashSet<>();
 
     static Dotenv dotenv = Dotenv.load();
 
     public static void main(String[] args) {
-        int period = 3600000; // 1 hour
-        Timer timer = new Timer();
+        List<String> oldData;
+        try {
+            oldData = readFile();
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
 
-        final ArrayList<String>[] newData = new ArrayList[]{new ArrayList<>()};
-        final ArrayList<String>[] oldData = new ArrayList[]{new ArrayList<>()};
-        timer.scheduleAtFixedRate(new TimerTask() {
-            public void run() {
-                newData[0] = FetchData();
+        List<String> newData;
+        newData = fetchData();
 
-                // Avoid the first run saying data changed
-                if (Objects.equals(oldData[0], new ArrayList<>())) {
-                    System.out.println("First run, no data to compare");
-                    oldData[0] = newData[0];
-                    return;
-                }
-
-                if (newData[0].equals(oldData[0])) {
-                    System.out.println("No change");
-                } else {
-                    System.out.println("Change detected");
-                    oldData[0] = newData[0];
-                    try {
-                        getRecipients();
-                        sendEmail();
-                    } catch (IOException | MessagingException e) {
-                        System.out.println("\033[0;31m" + "Error sending email:" + "\n" + e + "\033[0m");
-                    }
-                }
+        // Avoid the first run saying data changed
+        if (Objects.equals(oldData, null)) {
+            System.out.println("First run, no data to compare");
+            try {
+                saveToFile(newData);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
-        }, 0, period);
+            return;
+        }
+
+        if (newData.equals(oldData)) {
+            System.out.println("No change");
+        } else {
+            System.out.println("Change detected");
+            try {
+                saveToFile(newData);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                getRecipients();
+                sendEmail();
+            } catch (IOException | MessagingException e) {
+                System.out.println("\033[0;31m" + "Error sending email:" + "\n" + e + "\033[0m");
+            }
+        }
+    }
+
+    public static void saveToFile(List<String> array) throws IOException {
+        FileWriter fw = new FileWriter("schedule.txt");
+        for (var el : array) {
+            fw.write(el + "\n");
+        }
+        fw.close();
+    }
+
+    public static List<String> readFile() throws IOException, ClassNotFoundException {
+        var file = new File("schedule.txt");
+        if (!file.exists()) {
+            var result = file.createNewFile();
+            System.out.println("File created: " + result);
+            return null;
+        }
+
+        Scanner scanner = new Scanner(file);
+
+        List<String> array = new ArrayList<>();
+        while (scanner.hasNextLine()) {
+            var line = scanner.nextLine();
+            System.out.println(line + "\n");
+            array.add(line);
+        }
+
+        scanner.close();
+        return array;
     }
 
     public static void getRecipients() throws MessagingException, IOException {
-        Properties properties = System.getProperties();
+        var properties = System.getProperties();
         properties.put("mail.pop3.host", dotenv.get("POP_HOST"));
         properties.put("mail.pop3.port", dotenv.get("POP_PORT"));
         properties.put("mail.pop3.ssl.enable", "false");
         properties.put("mail.pop3.auth", "true");
-        Session session = Session.getInstance(properties, new javax.mail.Authenticator(){
+        var session = Session.getInstance(properties, new javax.mail.Authenticator(){
             protected PasswordAuthentication getPasswordAuthentication() {
                 return new PasswordAuthentication(dotenv.get("FROM_EMAIL"), dotenv.get("PASSWORD"));
             }
         });
-        Store store = session.getStore("pop3");
+        var store = session.getStore("pop3");
 
         store.connect(dotenv.get("HOST"), dotenv.get("FROM_EMAIL"), dotenv.get("PASSWORD"));
 
-        Folder emailFolder = store.getFolder("INBOX");
+        var emailFolder = store.getFolder("INBOX");
         emailFolder.open(Folder.READ_ONLY);
         System.out.println("Total Messages:" + emailFolder.getMessageCount());
         System.out.println(Arrays.toString(emailFolder.getMessages()));
@@ -82,16 +118,16 @@ public class Main {
     }
 
     public static void sendEmail() throws IOException {
-        String from = dotenv.get("FROM_EMAIL");
-        String host = dotenv.get("SMTP_HOST");
+        var from = dotenv.get("FROM_EMAIL");
+        var host = dotenv.get("SMTP_HOST");
 
-        Properties properties = System.getProperties();
+        var properties = System.getProperties();
         properties.put("mail.smtp.host", host);
         properties.put("mail.smtp.port", dotenv.get("SMTP_PORT"));
         properties.put("mail.smtp.ssl.enable", "false");
         properties.put("mail.smtp.auth", "true");
 
-        Session session = Session.getInstance(properties, new javax.mail.Authenticator(){
+        var session = Session.getInstance(properties, new javax.mail.Authenticator(){
             protected PasswordAuthentication getPasswordAuthentication() {
                 return new PasswordAuthentication(from, dotenv.get("PASSWORD"));
             }
@@ -103,7 +139,7 @@ public class Main {
                 return;
             }
 
-            MimeMessage message = new MimeMessage(session);
+            var message = new MimeMessage(session);
             message.setFrom(new InternetAddress(from));
 
             for (String recipient : recipients) {
@@ -113,7 +149,7 @@ public class Main {
 
             // All of these you can change to whatever you want
             message.setSubject("Rozvrh na VŠEM se změnil");
-            StringBuilder html = new StringBuilder();
+            var html = new StringBuilder();
             html.append("<html><body><h1>Rozvrh na VŠEM se změnil</h1><p>Nový rozvrh je na adrese: <a href=\"").append(getUrl()).append("\">").append(getUrl()).append("</a></p><p>Nový rozvrh:</p>");
             for (String table : HTMLTables) {
                 // This is to remove the unnecessary tables
@@ -125,20 +161,17 @@ public class Main {
             message.setContent(html.toString(), "text/html; charset=UTF-8");
 
             Transport.send(message);
-
-            System.out.println("Sent messages successfully...");
         } catch (MessagingException mex) {
             mex.printStackTrace();
         }
     }
 
-    public static ArrayList<String> FetchData() {
-        Document doc;
-        ArrayList<String> tables = new ArrayList<>();
+    public static List<String> fetchData() {
+        List<String> tables = new ArrayList<>();
         try {
-            doc = Jsoup.connect(getUrl()).get();
+            final var doc = Jsoup.connect(getUrl()).get();
             // You may need to change the selector to match your page
-            Elements newsHeadlines = doc.select("table");
+            final var newsHeadlines = doc.select("table");
             for (Element headline : newsHeadlines) {
                 tables.add(headline.text());
                 HTMLTables.add(headline.html());
@@ -164,7 +197,7 @@ public class Main {
 
     // You may not need this if you don't have a page with the next month's schedule
     public static String getMonth(boolean plusOne) {
-        String s = "";
+        var s = "";
         Month month;
         if (plusOne) {
             month = java.time.LocalDate.now().plusMonths(1).getMonth();
